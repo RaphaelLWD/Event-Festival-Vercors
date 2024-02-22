acc1e4466e90bc1d8237d05540da204f3355b96f
Tonie <tonie.lanquette.simplon@gmail.com>
2024-02-22 09:37:34 +0100

    odif class reservation

--- sources/classes/DataReservation.php
+++ sources/classes/DataReservation.php
@@ -15,11 +15,12 @@ final class DataReservation
         return $retour;
     }
 
-    public function getTicketR(): array {
+    public function getTicketR(): array
+    {
         $fichier = fopen($this->_DataR, "r");
         $ticket = [];
-        while(($ligne = fgetcsv($fichier, 500)) !=false){
-            $ticket[] = new Reservation($ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1], $ligne[1]);
+        while (($ligne = fgetcsv($fichier, 500)) != false) {
+            $ticket[] = new Reservation($ligne[2], $ligne[3], $ligne[4], $ligne[5], $ligne[6], $ligne[7], $ligne[8], $ligne[9], $ligne[10], $ligne[17], $ligne[12], $ligne[13], $ligne[14], $ligne[15], $ligne[0]);
         }
         fclose($fichier);
         return $ticket;
--- sources/traitement.php
+++ sources/traitement.php
@@ -1,1 +1,8 @@
 <?php
+if(isset ($_POST["nom"]) && !empty($_POST["nom"])){
+    $nom = htmlentities(($_POST["nom"]));
+
+}else{
+    header("location:./index.php?erreur=". ERREUR_NOM);
+    die;
+}
